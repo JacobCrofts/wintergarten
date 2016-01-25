c79f3807486c66214c6c5357c20ba5be31465612
Jacob Crofts & Michael Jasinski <pair+jacobcrofts.michael-jas@devbootcamp.com>
2016-01-25 17:54:18 -0600

    Add title field to review table

--- db/migrate/20160125222418_create_reviews.rb
+++ db/migrate/20160125222418_create_reviews.rb
@@ -3,6 +3,7 @@ class CreateReviews < ActiveRecord::Migration
     create_table :reviews do |t|
       t.integer :film_id
       t.integer :reviewer_id
+      t.string :title
       t.text :content
 
       t.timestamps null: false
