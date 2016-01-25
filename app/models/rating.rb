30d10a6cc651e4b42f957a8f390c20c2667a4b83
Jacob Crofts & Michael Jasinski <pair+jacobcrofts.michael-jas@devbootcamp.com>
2016-01-25 17:51:27 -0600

    Finish associations (untested)

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,5 +1,4 @@
 class Rating < ActiveRecord::Base
   belongs_to :rateable, polymorphic: true
   belongs_to :rater, class_name: "User"
-
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,6 @@
 class Review < ActiveRecord::Base
+  has_many :comments, as: :commentable
+  has_many :ratings, as: :ratable
+  belongs_to :film
+  belongs_to :reviewer, class_name: "User"
 end
