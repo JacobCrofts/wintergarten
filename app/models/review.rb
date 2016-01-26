9116f18f31f3abd30d97868181d06937c942b77e
Jacob Crofts & Michael Jasinski <pair+jacobcrofts.michael-jas@devbootcamp.com>
2016-01-25 18:29:56 -0600

    Complete and test database associations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ActiveRecord::Base
   has_many :comments, as: :commentable
-  has_many :ratings, as: :ratable
+  has_many :ratings, as: :rateable
   belongs_to :film
   belongs_to :reviewer, class_name: "User"
 end
