605ec408e0a00d97985fda189663af9bed403ecd
Jacob Crofts & Michael Jasinski <pair+jacobcrofts.michael-jas@devbootcamp.com>
2016-01-26 14:21:14 -0600

    Change comment form to partial

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,15 @@ class CommentsController < ApplicationController
 
   def new
     @comment = Comment.new(comment_params)
+
+    puts @comment
+    puts comment_params
+    puts '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
+
   end
 
   def comment_parms
-    params.require(:comment).permit(:content, :commentable_id, :commentable_type)
+    params.require(:comment).permit(:content, :commentable_id, :commentable_type, :commenter_id                                           )
   end
 
 
