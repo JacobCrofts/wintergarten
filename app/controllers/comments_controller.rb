411c2ec0514326818861eb0c682a97d9823128b2
Jacob Crofts & Michael Jasinski <pair+jacobcrofts.michael-jas@devbootcamp.com>
2016-01-26 15:29:42 -0600

    Add create method for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,26 @@
 class CommentsController < ApplicationController
 
-  def new
-    @comment = Comment.new(comment_params)
 
-    puts @comment
-    puts comment_params
-    puts '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
+  def create
+    # user
+    user = User.find_by(id: session[:user_id])
+    # commentable
+    if params[:film_id]
+      commentable = Film.find(params[:film_id])
+    else
+      commentable = Review.find(params[:review_id])
+    end
 
+    @comment = commentable.comments.build(comment_params.merge(commenter: user))
+    if @comment.save
+      redirect_to commentable
+    else
+      render commentable
+    end
   end
 
-  def comment_parms
-    params.require(:comment).permit(:content, :commentable_id, :commentable_type, :commenter_id                                           )
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 
 
