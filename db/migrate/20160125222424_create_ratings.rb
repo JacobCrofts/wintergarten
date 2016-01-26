4c438ab9205eb0d3cb4e8c83f81cd1ca7e36ab10
Jacob Crofts & Michael Jasinski <pair+jacobcrofts.michael-jas@devbootcamp.com>
2016-01-25 18:42:48 -0600

    Add indexing to all foreign keys

--- db/migrate/20160125222424_create_ratings.rb
+++ db/migrate/20160125222424_create_ratings.rb
@@ -1,8 +1,8 @@
 class CreateRatings < ActiveRecord::Migration
   def change
     create_table :ratings do |t|
-      t.integer :rateable_id
-      t.integer :rater_id
+      t.integer :rateable_id, index: true
+      t.integer :rater_id, index: true
       t.string :rateable_type
       t.integer :value
 
