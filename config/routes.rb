ea647a0d1965293e08d7364d96ddb5d7fa525864
John Doe <johndoe@example.com>
2016-01-26 14:55:56 -0600

    Fix bug: user cannot logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get '/login' => 'sessions#new', as: 'login'
   post '/login' => 'sessions#create'
-  delete '/logout' => 'sessions#destroy', as: 'logout'
+  get '/logout' => 'sessions#destroy', as: 'logout'
 
   resources :films, :only => [:index, :show] do
     resources :comments, :only => [:create, :update, :destroy]
