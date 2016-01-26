60a0837fb4ba5a6f90d2be2673bd7ea08d2ba4f0
Jacob Crofts & Michael Jasinski <pair+jacobcrofts.michael-jas@devbootcamp.com>
2016-01-25 21:06:57 -0600

    Add rails and spec helpers, one test green

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  let(:user) { User.new}
+
+  it "creates a new user" do
+    user1 = User.create!(id: 1, username: "mj", password_digest: "mj", email: "mj")
+    expect(user1).to be_a(User)
+  end
+
+# RSpec.describe User, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
 end
