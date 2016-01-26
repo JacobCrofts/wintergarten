06790dd950e9edf3bcdd06e6a5b990951f07b753
Jacob Crofts & Michael Jasinski <pair+jacobcrofts.michael-jas@devbootcamp.com>
2016-01-26 09:50:17 -0600

    Add average score method and display ratings in film

--- app/models/film.rb
+++ app/models/film.rb
@@ -2,4 +2,11 @@ class Film < ActiveRecord::Base
   has_many :comments, as: :commentable
   has_many :reviews
   has_many :ratings, as: :rateable
+
+
+  def average_score
+    self.ratings.reduce(0){|sum, rating| sum + rating.value } / self.ratings.length.to_f
+  end
+
+
 end
